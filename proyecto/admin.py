1023227136653b82b5b62edbbfdf736789439054
Sergio <servesa07@gmail.com>
2023-03-31 00:23:55 -0400

    Sergio Veizaga Sanchez

--- proyecto/admin.py
+++ proyecto/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Project, Task, Laptop
+
+# Register your models here.
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(Laptop)
--- proyecto/models.py
+++ proyecto/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title + ' - ' + self.project.name
+    
+
+class Laptop(models.Model):
+    marca = models.CharField(max_length=20,unique=True)
+    modelo = models.CharField(max_length=20,unique=True)
+    stock = models.IntegerField()
+    precio = models.IntegerField()
+    pantalla = models.FloatField()
+    teclado = models.CharField(max_length=10,unique=True)
+    procesador = models.CharField(max_length=25,unique=True)
+    ram = models.IntegerField()
+    color = models.CharField(max_length=15,unique=True)
+    m2 = models.IntegerField()
+    hdd = models.IntegerField()
+    grafica = models.CharField(max_length=25,unique=True)
+    descripcion = models.CharField(max_length=300,unique=True)
+    imagenes = models.ImageField(upload_to="imagenes/productos", null=True)
+
