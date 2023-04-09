9376c011d927e95c28ac4fa54b15cb2346c20b3b
adan llanos <adanllanos96@gmail.com>
2023-04-09 18:08:06 -0400

    cambio de orden para verificar los pixeles V2

--- proyecto/static/scripts/limitarpxl.js
+++ proyecto/static/scripts/limitarpxl.js
@@ -1,23 +1,4 @@
-var fileUpload = document.querySelector('input[name="imagen_1"]')
-var reader = new FileReader();
-fileUpload.addEventListener('change', () => {
-  reader.readAsDataURL(fileUpload.files[0]);
-  reader.onload = (e) => {
-    var image = new Image();
-    image.src = e.target.result;
-    image.onload = () => {
-      var {
-        height,
-        width
-      } = image;
-      if (height < 1080 || width < 720) {
-        alert("La resolucion minima de la imagen debe ser 1080x720");
-        return false;
-      }
-      alert("Imagen aceptada!");
-      return true;
-    };
-  };})
+
 var fileUpload = document.querySelector('input[name="imagen_2"]')
 var reader = new FileReader();
 fileUpload.addEventListener('change', () => {
@@ -38,6 +19,29 @@ fileUpload.addEventListener('change', () => {
       return true;
     };
   };})
+
+  var fileUpload = document.querySelector('input[name="imagen_4"]')
+var reader = new FileReader();
+fileUpload.addEventListener('change', () => {
+  reader.readAsDataURL(fileUpload.files[0]);
+  reader.onload = (e) => {
+    var image = new Image();
+    image.src = e.target.result;
+    image.onload = () => {
+      var {
+        height,
+        width
+      } = image;
+      if (height < 1080 || width < 720) {
+        alert("La resolucion minima de la imagen debe ser 1080x720");
+        return false;
+      }
+      alert("Imagen aceptada!");
+      return true;
+    };
+  };})
+  
+
   var fileUpload = document.querySelector('input[name="imagen_3"]')
 var reader = new FileReader();
 fileUpload.addEventListener('change', () => {
@@ -58,7 +62,9 @@ fileUpload.addEventListener('change', () => {
       return true;
     };
   };})
-  var fileUpload = document.querySelector('input[name="imagen_4"]')
+
+
+  var fileUpload = document.querySelector('input[name="imagen_1"]')
 var reader = new FileReader();
 fileUpload.addEventListener('change', () => {
   reader.readAsDataURL(fileUpload.files[0]);
