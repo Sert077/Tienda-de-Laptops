9465fa1f2aae2643970f360f7c28c040c7bb3467
victordelgadorocha <victorhugodelgado911@gmail.com>
2023-04-21 22:10:09 -0400

    boton eliminar cambios

---
+++ proyecto/static/scripts/limitarpxl.js
@@ -0,0 +1,86 @@
+
+var fileUpload = document.querySelector('input[name="imagen_2"]')
+var reader = new FileReader();
+fileUpload.addEventListener('change', () => {
+  reader.readAsDataURL(fileUpload.files[0]);
+  reader.onload = (e) => {
+    var image = new Image();
+    image.src = e.target.result;
+    image.onload = () => {
+      var {
+        height,
+        width
+      } = image;
+      if (height < 1080 || width < 720) {
+        alert("La resolucion minima de la imagen debe ser 1080x720");
+        return false;
+      }
+      alert("Imagen aceptada!");
+      return true;
+    };
+  };})
+
+  var fileUpload = document.querySelector('input[name="imagen_4"]')
+var reader = new FileReader();
+fileUpload.addEventListener('change', () => {
+  reader.readAsDataURL(fileUpload.files[0]);
+  reader.onload = (e) => {
+    var image = new Image();
+    image.src = e.target.result;
+    image.onload = () => {
+      var {
+        height,
+        width
+      } = image;
+      if (height < 1080 || width < 720) {
+        alert("La resolucion minima de la imagen debe ser 1080x720");
+        return false;
+      }
+      alert("Imagen aceptada!");
+      return true;
+    };
+  };})
+  
+
+  var fileUpload = document.querySelector('input[name="imagen_3"]')
+var reader = new FileReader();
+fileUpload.addEventListener('change', () => {
+  reader.readAsDataURL(fileUpload.files[0]);
+  reader.onload = (e) => {
+    var image = new Image();
+    image.src = e.target.result;
+    image.onload = () => {
+      var {
+        height,
+        width
+      } = image;
+      if (height < 1080 || width < 720) {
+        alert("La resolucion minima de la imagen debe ser 1080x720");
+        return false;
+      }
+      alert("Imagen aceptada!");
+      return true;
+    };
+  };})
+
+
+  var fileUpload = document.querySelector('input[name="imagen_1"]')
+var reader = new FileReader();
+fileUpload.addEventListener('change', () => {
+  reader.readAsDataURL(fileUpload.files[0]);
+  reader.onload = (e) => {
+    var image = new Image();
+    image.src = e.target.result;
+    image.onload = () => {
+      var {
+        height,
+        width
+      } = image;
+      if (height < 1080 || width < 720) {
+        alert("La resolucion minima de la imagen debe ser 1080x720");
+        return false;
+      }
+      alert("Imagen aceptada!");
+      return true;
+    };
+  };})
---
+++ proyecto/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.conf.urls import handler404
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('laptops/', views.laptops, name="laptops"),
+    path('logout/', views.signout, name='logout'),
+    path('login/', views.signin, name="signin"),
+    path('informacionLaptop/<int:id>',views.informacionLaptop, name = "informacionLaptop"),
+    path('modificarLaptop/<int:id>',views.modificarLaptop, name = "modificarLaptop"),
+
+]
+
+
+handler404 = 'proyecto.views.error_404'
