b95c1f9df99711d043de3e4117bc75ea0c91d1dc
Sert077 <servesa07@gmail.com>
2023-06-13 20:12:55 -0400

    factura datos corregido

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -7,8 +7,16 @@ urlpatterns = [
     path('logout/', views.signout, name='logout'),
     path('login/', views.signin, name="signin"),
     path('informacionLaptop/<int:id>',views.informacionLaptop, name = "informacionLaptop"),
-
-
+    path('modificarLaptop/<int:id>',views.modificarLaptop, name = "modificarLaptop"),
+    path('venderLaptop/<int:id>',views.venderLaptop, name = "venderLaptop"),
+    path('registroVentas/',views.registroVentas, name = "registroVentas"),
+    path('reporteVentas/',views.reporteVentas, name = "reporteVentas"),
+    path('registroFacturas/',views.registroFacturas, name = "registroFacturas"),
+    path('usuarios/',views.usuarios, name = "usuarios"),
+    path('eliminarUsuario/<int:id>', views.eliminarUsuario, name="eliminarUsuario"),
+    path('registro/', views.registro, name="registro"),
+    path('factura/', views.factura, name="factura")
+    
 ]
 
 
